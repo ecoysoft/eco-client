2e5282d0fa44d5b9afe2c78ee7b48cbda5e03cba
Nandini Nila <nandininila1@gmail.com>
2024-03-31 16:47:42 +0600

    AddFeat: TopNav Added

--- app/Layouts/layout.tsx
+++ app/Layouts/layout.tsx
@@ -1,20 +1,20 @@
-import { Box } from "@mui/material";
+import { Grid } from "@mui/material";
 import { Footer } from "./footer";
 import { TopNav } from "./top-nav";
 
 export const Layout = (props: any) => {
   return (
-    <Box
-      display={"flex"}
-      flexDirection={"column"}
-      justifyContent={"space-between"}
+    <Grid
+      container
+      direction="column"
+      justifyContent="space-between"
+      alignItems="center"
       height={"100vh"}
       width={"100%"}
-      maxWidth={"1440px"}
     >
       <TopNav />
       {props.children}
       <Footer />
-    </Box>
+    </Grid>
   );
 };
