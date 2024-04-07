03cfbe82a60970e19cd2da782ff5d09f5803f754
Nandini Nila <nandininila1@gmail.com>
2024-04-08 03:07:49 +0600

    Fix: dark overlay

--- app/sections/home/why-choose-us.tsx
+++ app/sections/home/why-choose-us.tsx
@@ -122,11 +122,11 @@ export const WhyChooseUs = () => {
                 backgroundImage: `url("https://images.unsplash.com/photo-1551135049-8a33b5883817?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`,
                 backgroundSize: "cover",
                 position: "absolute",
+                filter: "brightness(0.5)",
                 top: 0,
                 right: 0,
                 bottom: 0,
                 left: 0,
-                opacity: "0.7",
                 borderRadius: 3,
               },
 
@@ -139,6 +139,7 @@ export const WhyChooseUs = () => {
             <Typography
               position={"relative"}
               textAlign={"center"}
+              color={"white"}
               textTransform={"uppercase"}
               typography={{ xs: "h5", md: "h4" }}
               style={{ fontWeight: 700, letterSpacing: 5 }}
